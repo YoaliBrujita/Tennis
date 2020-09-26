0b1d64de7509bf556c321c156de7aa9fb568a883
Yoali Soto <40327454+YoaliBrujita@users.noreply.github.com>
2020-09-25 21:50:39 -0500

    Add files via upload

---
+++ js/mis-js.js
@@ -0,0 +1,71 @@
+$(document).ready(main);
+var contador = 1;
+function main(){
+	$('.menu_bar').click(function(){
+ 
+		if(contador == 1){
+			$('nav').animate({
+				right: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				right: '-100%'
+			});
+		}
+ 
+	});
+ 
+};
+
+/*slider texto*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("carrusel");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+$(document).ready(function(){
+    $('.div-productos').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
+    });
+});
